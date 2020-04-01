840cbfdd173b0f6deb2f1d2a27b1c1f81dc999db
Rayan <rayansud@stanford.edu>
2020-04-01 23:00:39 +0530

    Added all Django server code
    Currently provides questionnaire handling and stores API location requests as CSVs

---
+++ Server Code/reportLocation/intersection_calculator.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import descartes
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
---
+++ Server Code/reportLocation/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.utils.text import slugify
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
+
+# Create your views here.
+def main(request):
+    if request.method=='POST':
+        locationData = request.POST
+        locationHistory = locationData['locationHistory']
+        for row in locationHistory:
+            with open("LocationData/"+slugify(timeslot)+".csv","a+") as timeslot_file:
+                timeslot_file.write(locationData['uuid'],locationData['status'],row['lat'],row['lng'])
+        return HttpResponse('API Request Received OK',statuscode=200)
+    else:
+        return HttpResponse('Failed')
